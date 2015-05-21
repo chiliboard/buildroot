f561505c296cf9495dcbc161bb0633d76fb08410
Marcin Niestroj <m.niestroj@grinn-global.com>
2015-05-21 23:35:56 +0200

    boards/grinn: added support for chiliboard (chiliboard.org)
    Repository includes links to official TI Linux kernel repo and official u-boot
    bootloader. Additionally patches and firmware files and configuration is
    provided to support am335x-based chiliboard (chiliboard.org).

---
+++ board/grinn/chiliboard/post-build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#post-build script for chiliboard
+
+cp board/grinn/chiliboard/uEnv.txt ${BINARIES_DIR}/uEnv.txt
+cp board/grinn/chiliboard/mkcard.sh ${BINARIES_DIR}/mkcard.sh
+
+install -m 0644 -D ${BINARIES_DIR}/*.dtb ${TARGET_DIR}/boot/am335x-chiliboard.dtb
+
+install -m 0644 -D board/grinn/chiliboard/firmware/am335x-pm-firmware.elf ${TARGET_DIR}/lib/firmware/am335x-pm-firmware.elf
+install -m 0644 -D board/grinn/chiliboard/firmware/am335x-bone-scale-data.bin ${TARGET_DIR}/lib/firmware/am335x-bone-scale-data.bin
+
+install -m 0644 -D board/grinn/chiliboard/70-power-switch.rules ${TARGET_DIR}/lib/udev/rules.d/70-power-switch.rules
