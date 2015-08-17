5dc2559e7d61bad12b78f35929c908923bfa8afa
Wojciech Nizinski <w.nizinski@grinn-global.com>
2015-08-17 13:46:25 +0200

    input-event-daemon to handle power button

--- board/grinn/chiliboard/post-build.sh
+++ board/grinn/chiliboard/post-build.sh
@@ -11,6 +11,7 @@ install -m 0644 -D ${BINARIES_DIR}/*.dtb ${TARGET_DIR}/boot/am335x-chiliboard.dt
 install -m 0644 -D board/grinn/chiliboard/firmware/am335x-pm-firmware.elf ${TARGET_DIR}/lib/firmware/am335x-pm-firmware.elf
 install -m 0644 -D board/grinn/chiliboard/firmware/am335x-bone-scale-data.bin ${TARGET_DIR}/lib/firmware/am335x-bone-scale-data.bin
 
+install -m 0644 -D board/grinn/chiliboard/input-event-daemon.conf ${TARGET_DIR}/etc/input-event-daemon.conf
 install -m 0644 -D board/grinn/chiliboard/70-power-switch.rules ${TARGET_DIR}/lib/udev/rules.d/70-power-switch.rules
 
 install -m 0775 -D board/grinn/chiliboard/ti-gfx ${TARGET_DIR}/usr/sbin/ti-gfx
